e10e968df3a40896055d46b8e482832993bf3530
krist <krist@hellowala.org>
2011-04-21 19:20:43 +0800

    build ok , pref pane and app

---
+++ SneakyPreferencesPref.h
@@ -0,0 +1,19 @@
+//
+//  SneakyPreferencesPref.h
+//  SneakyPreferences
+//
+//  Created by Krist Menina on 4/21/11.
+//  Copyright (c) 2011 Hello Wala Studios. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface SneakyPreferencesPref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ SneakyPreferencesPref.m
@@ -0,0 +1,18 @@
+//
+//  SneakyPreferencesPref.m
+//  SneakyPreferences
+//
+//  Created by Krist Menina on 4/21/11.
+//  Copyright (c) 2011 Hello Wala Studios. All rights reserved.
+//
+
+#import "SneakyPreferencesPref.h"
+
+
+@implementation SneakyPreferencesPref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
